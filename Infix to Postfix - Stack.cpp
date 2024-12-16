91c630b5747c48ddc76831797b7506fb21248414
Kisuke Urahara <97740559+XxYadavxX@users.noreply.github.com>
2024-12-16 21:55:01 +0530

    Update Infix to Postfix - Stack.cpp

--- Infix to Postfix - Stack.cpp
+++ Infix to Postfix - Stack.cpp
@@ -14,25 +14,24 @@ OUTPUT:
 	1) Display postfix expression
 	
 
-AUTHOR:  Sunil
-DATE: 15.10.2024
+
 */
 #include<iostream>
 #include<string.h>
 #define MAX 10
 using namespace std;
 
-//ADT Stack
+
 class Stack{
 	private:
-		char S[MAX];//Stack array
-		int top;//pointer to the top element of a stack
+		char S[MAX];
+		int top;
 	public:
 		Stack(){
-			top=-1;//Stack empty means top points to invalid location
+			top=-1;
 		}
 		
-		//check if stack is empty
+		
 		bool isStackEmpty(){
 			if (top == -1)
 				return true;
@@ -40,7 +39,7 @@ class Stack{
 				return false;
 		}
 		
-		//check if stack is full
+		
 		bool isStackFull(){
 			if (top == MAX-1)
 				return true;
@@ -48,7 +47,7 @@ class Stack{
 				return false;
 		}
 		
-		//Add element in stack 'Push Operation'
+		
 		void push(char token){
 				if(! isStackFull())
 					S[++top]=token;
@@ -56,7 +55,7 @@ class Stack{
 					cout<<"\nStack is full!!";
 		}
 		
-		//Remove element from stack 'Pop Operation'
+		
 		char pop(){
 				if(!isStackEmpty()){
 					cout<<"\n"<<S[top]<<"  is popped from Stack";
@@ -68,12 +67,12 @@ class Stack{
 				return '\0';
 		}
 		
-		//Top()
+		
 		char Top(){
 			return S[top];
 		}
 		
-		//Display stack contents
+		
 		void displayStack(){
 			int i;
 			if(!isStackEmpty()){
@@ -86,10 +85,10 @@ class Stack{
 };
 
 
-//Implementation class
+
 class ExpressionConversion{
 	private:
-		char E[20];//an array to store an infix expression
+		char E[20];
 		Stack stk;
 	public:
 		ExpressionConversion(){
@@ -104,7 +103,7 @@ class ExpressionConversion{
 			cout<<"\nThe input infix expression is:"<<E;
 		}
 		
-		//Function to calculate Incoming priority of an operator
+		
 		int ICP(char t)
 		{
 			int v;
@@ -128,7 +127,7 @@ class ExpressionConversion{
 			return v;
 		}
 		
-		//Function to calculate In-Stack priority of an operator
+		
 		int ISP(char t)
 		{
 			int v;
@@ -152,59 +151,59 @@ class ExpressionConversion{
 			return v;
 		}
 	
-		//Function to perform the infix to postfix
+		
 		void in2Post()
 		{
 			
 			char POST[20];
 			int i,j=0;
 			i=0;
-			//Till end of the expression
+			
 			while(E[i] != '\0')
 			{
 				if(E[i]=='+' || E[i]=='-' ||E[i]=='*' ||E[i]=='/' |E[i]=='^' ||E[i]=='(')
 				{
-					//if stack is empty then push the operator unconditionally
+					
 					if(stk.isStackEmpty())
 					{
 						stk.push(E[i]);
 					}
 					else
 					{
-						//Pop all the operators in stack 
-						//having priority >= to the incoming operator priority
+						
+						
 						while(ICP(E[i]) <= ISP(stk.Top()) && !stk.isStackEmpty())
 							POST[j++]=stk.pop();
 			  
-						stk.push(E[i]);//Push the incoming operator unconditionally
+						stk.push(E[i]);
 					}
 				}
 				else
-					if(E[i] == ')')//if right bracket comes then pop everything from the stack till '(' comes
+					if(E[i] == ')')
 					{
 						while(stk.Top() != '(')
 							POST[j++]=stk.pop();
 						
-						stk.pop();//pop ')' from the stack
+						stk.pop();
 					}
 					else
-						POST[j++]=E[i];//the token is operand, so push it on stack
+						POST[j++]=E[i];
 
-					i++;//go to the next token in the expression
+					i++;
 			}
   
-			//After scanning entire expression pop everything from stack and save it to the output expression
+			
 			while(!stk.isStackEmpty())
-				POST[j++]=stk.pop();//here I wrote E
+				POST[j++]=stk.pop();
   
-			POST[j]='\0';//Store end of the expression 'NULL' character at the end
-			//print the Postfix expression
+			POST[j]='\0';
+			
 			cout<<"\nThe prefix expression is ::"<<POST;
 		}
 
-};//end class
+};
 
-//Driver Code
+
 int main(){
 	int choice=0;
 
@@ -232,7 +231,7 @@ int main(){
 				cout<<"\nGood By!!";
 				break;
 			
-		}//end switch
-	}//end while
+		}
+	}
 	return 0;
-}//end main
+}
